810a0ff4e69d2652aa0bdac563e65433f86561fb
Jamie <wangqishere@gmail.com>
2021-08-15 15:25:15 -0700

    fix gorm not working with []string, use json to store urls

--- server/resolver/post.resolvers.go
+++ server/resolver/post.resolvers.go
@@ -5,6 +5,7 @@ package resolver
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/Luismorlan/newsmux/model"
@@ -15,7 +16,31 @@ func (r *postResolver) DeletedAt(ctx context.Context, obj *model.Post) (*time.Ti
 	return &obj.DeletedAt.Time, nil
 }
 
+func (r *postResolver) ImageUrls(ctx context.Context, obj *model.Post) ([]string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *postResolver) FileUrls(ctx context.Context, obj *model.Post) ([]string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
 // Post returns generated.PostResolver implementation.
 func (r *Resolver) Post() generated.PostResolver { return &postResolver{r} }
 
 type postResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *postResolver) CrawledAt(ctx context.Context, obj *model.Post) (*time.Time, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+func (r *postResolver) ContentGeneratedAt(ctx context.Context, obj *model.Post) (*time.Time, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+func (r *postResolver) OriginURL(ctx context.Context, obj *model.Post) (*string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
