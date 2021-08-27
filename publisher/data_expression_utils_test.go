6db9883e05b63e3aaf1755b228188dd5f483ceae
Weilun Chen <chenweilunster@gmail.com>
2021-08-26 20:03:19 -0700

    minor refactor for data expression (#27)
    * refactor data expression
    
    * refactor comment

--- publisher/data_expression_utils_test.go
+++ publisher/data_expression_utils_test.go
@@ -2,7 +2,6 @@ package publisher
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 
 	"github.com/Luismorlan/newsmux/model"
@@ -15,59 +14,132 @@ const (
 	// please also change this in order to keep unit test still alive
 	jsonStringForTest = `
 	{
-		"dataExpression":{
-		   "id":"1",
-		   "expr":{
-			  "allOf":[
-				 {
-					"id":"1.1",
-					"expr":{
-					   "anyOf":[
-						  {
-							 "id":"1.1.1",
-							 "expr":{
-								"pred":{
-								   "type":"LITERAL",
-								   "param":{
-									  "text":"bitcoin"
-								   }
-								}
-							 }
-						  },
-						  {
-							 "id":"1.1.2",
-							 "expr":{
-								"pred":{
-								   "type":"LITERAL",
-								   "param":{
-									  "text":"以太坊"
-								   }
-								}
-							 }
-						  }
-					   ]
-					}
-				 },
-				 {
-					"id":"1.2",
-					"expr":{
-					   "notTrue":{
-						  "id":"1.2.1",
-						  "expr":{
-							 "pred":{
+		"id":"1",
+		"expr":{
+		"allOf":[
+			{
+			"id":"1.1",
+			"expr":{
+					"anyOf":[
+					{
+						"id":"1.1.1",
+						"expr":{
+						"pred":{
 								"type":"LITERAL",
 								"param":{
-								   "text":"马斯克"
+								"text":"bitcoin"
 								}
-							 }
-						  }
-					   }
+						}
+						}
+					},
+					{
+						"id":"1.1.2",
+						"expr":{
+							"pred":{
+									"type":"LITERAL",
+									"param":{
+										"text":"以太坊"
+									}
+							}
+						}
 					}
-				 }
-			  ]
-		   }
+				]
+			}
+			},
+			{
+				"id":"1.2",
+				"expr":{
+						"notTrue":{
+							"id":"1.2.1",
+							"expr":{
+								"pred":{
+									"type":"LITERAL",
+									"param":{
+										"text":"马斯克"
+									}
+								}
+							}
+						}
+					}
+				}
+			]
 		}
-	 }
+	}
+	`
+
+	jsonWithEmptyExpression = `
+	{
+		"id": "1"
+	}
+	`
+
+	jsonWithPureIdExpression = `
+	{
+		"id":"1",
+		"expr":{
+			"allOf":[
+				{
+					"id":"1.1",
+					"expr":{
+						"anyOf":[
+							{
+								"id":"1.1.1",
+								"expr":{
+									"pred":{
+										"type":"LITERAL",
+										"param":{
+											"text":"bitcoin"
+										}
+									}
+								}
+							},
+							{
+								"id":"1.1.2",
+								"expr":{
+									"pred":{
+										"type":"LITERAL",
+										"param":{
+											"text":"以太坊"
+										}
+									}
+								}
+							},
+							{
+								"id": "1.1.3",
+								"expr": {
+									"notTrue": {
+										"id": "1.1.3.1"
+									}
+								}
+							},
+							{
+								"id": "1.1.4"
+							}
+						]
+					}
+				},
+				{
+					"id":"1.2",
+					"expr":{
+						"notTrue":{
+							"id":"1.2.1",
+							"expr":{
+								"pred":{
+									"type":"LITERAL",
+									"param":{
+										"text":"马斯克"
+									}
+								}
+							}
+						}
+					}
+				}, 
+				{
+					"id": "1.3"
+				}
+			]
+		}
+	}
 	`
 )
 
@@ -75,65 +147,61 @@ func TestDataExpressionUnmarshal(t *testing.T) {
 	t.Run("Test unmarshal 1", func(t *testing.T) {
 		jsonStr := jsonStringForTest
 		// Check  marshal - unmarshal are consistent
-		var root model.DataExpressionRoot
-		json.Unmarshal([]byte(jsonStr), &root)
-		fmt.Printf("%+v\n", root)
+		var dataExpressionWrap model.DataExpressionWrap
+		json.Unmarshal([]byte(jsonStr), &dataExpressionWrap)
 
-		bytes, _ := json.Marshal(root)
-		var newRoot model.DataExpressionRoot
+		bytes, _ := json.Marshal(dataExpressionWrap)
+		var newDataExpressionWrap model.DataExpressionWrap
 
-		json.Unmarshal(bytes, &newRoot)
-		fmt.Printf("%+v\n", newRoot)
+		json.Unmarshal(bytes, &newDataExpressionWrap)
 
-		newBytes, _ := json.Marshal(root)
+		newBytes, _ := json.Marshal(newDataExpressionWrap)
 
-		require.True(t, cmp.Equal(root, newRoot))
+		require.True(t, cmp.Equal(dataExpressionWrap, newDataExpressionWrap))
 		require.Equal(t, bytes, newBytes)
 	})
 }
 
 func TestDataExpressionMatch(t *testing.T) {
 	t.Run("Test matching function", func(t *testing.T) {
-		var root = model.DataExpressionRoot{
-			Root: model.DataExpressionWrap{
-				ID: "1",
-				Expr: model.AllOf{
-					AllOf: []model.DataExpressionWrap{
-						{
-							ID: "1.1",
-							Expr: model.AnyOf{
-								AnyOf: []model.DataExpressionWrap{
-									{
-										ID: "1.1.1",
-										Expr: model.PredicateWrap{
-											Predicate: model.Predicate{
-												Type:  "LITERAL",
-												Param: model.Literal{"bitcoin"},
-											},
+		var dataExpressionWrap = model.DataExpressionWrap{
+			ID: "1",
+			Expr: model.AllOf{
+				AllOf: []model.DataExpressionWrap{
+					{
+						ID: "1.1",
+						Expr: model.AnyOf{
+							AnyOf: []model.DataExpressionWrap{
+								{
+									ID: "1.1.1",
+									Expr: model.PredicateWrap{
+										Predicate: model.Predicate{
+											Type:  "LITERAL",
+											Param: model.Literal{"bitcoin"},
 										},
 									},
-									{
-										ID: "1.1.2",
-										Expr: model.PredicateWrap{
-											Predicate: model.Predicate{
-												Type:  "LITERAL",
-												Param: model.Literal{"以太坊"},
-											},
+								},
+								{
+									ID: "1.1.2",
+									Expr: model.PredicateWrap{
+										Predicate: model.Predicate{
+											Type:  "LITERAL",
+											Param: model.Literal{"以太坊"},
 										},
 									},
 								},
 							},
 						},
-						{
-							ID: "1.2",
-							Expr: model.NotTrue{
-								NotTrue: model.DataExpressionWrap{
-									ID: "1.2.1",
-									Expr: model.PredicateWrap{
-										Predicate: model.Predicate{
-											Type:  "LITERAL",
-											Param: model.Literal{"马斯克"},
-										},
+					},
+					{
+						ID: "1.2",
+						Expr: model.NotTrue{
+							NotTrue: model.DataExpressionWrap{
+								ID: "1.2.1",
+								Expr: model.PredicateWrap{
+									Predicate: model.Predicate{
+										Type:  "LITERAL",
+										Param: model.Literal{"马斯克"},
 									},
 								},
 							},
@@ -143,29 +211,24 @@ func TestDataExpressionMatch(t *testing.T) {
 			},
 		}
 
-		bytes, _ := json.Marshal(root)
+		bytes, _ := json.Marshal(dataExpressionWrap)
 
-		var res model.DataExpressionRoot
+		var res model.DataExpressionWrap
 		json.Unmarshal(bytes, &res)
 
-		fmt.Printf("%+v \n", res.Root.Expr)
-
-		bytes2, _ := json.Marshal(res)
-		fmt.Println(string(bytes2))
-
-		matched, err := DataExpressionMatch(res.Root, model.Post{Content: "马斯克做空以太坊"})
+		matched, err := DataExpressionMatch(res, model.Post{Content: "马斯克做空以太坊"})
 		require.Nil(t, err)
 		require.Equal(t, false, matched)
 
-		matched, err = DataExpressionMatch(res.Root, model.Post{Content: "老王做空以太坊"})
+		matched, err = DataExpressionMatch(res, model.Post{Content: "老王做空以太坊"})
 		require.Nil(t, err)
 		require.Equal(t, true, matched)
 
-		matched, err = DataExpressionMatch(res.Root, model.Post{Content: "老王做空比特币"})
+		matched, err = DataExpressionMatch(res, model.Post{Content: "老王做空比特币"})
 		require.Nil(t, err)
 		require.Equal(t, false, matched)
 
-		matched, err = DataExpressionMatch(res.Root, model.Post{Content: "老王做空bitcoin"})
+		matched, err = DataExpressionMatch(res, model.Post{Content: "老王做空bitcoin"})
 		require.Nil(t, err)
 		require.Equal(t, true, matched)
 	})
@@ -187,5 +250,37 @@ func TestDataExpressionMatch(t *testing.T) {
 		matched, err = DataExpressionMatchPost(jsonStringForTest, model.Post{Content: "老王做空bitcoin"})
 		require.Nil(t, err)
 		require.Equal(t, true, matched)
+	})
+
+	t.Run("Test matching from json string with pure id expression", func(t *testing.T) {
+		matched, err := DataExpressionMatchPost(jsonWithPureIdExpression, model.Post{Content: "马斯克做空以太坊"})
+		require.Nil(t, err)
+		require.Equal(t, false, matched)
+
+		matched, err = DataExpressionMatchPost(jsonWithPureIdExpression, model.Post{Content: "老王做空以太坊"})
+		require.Nil(t, err)
+		require.Equal(t, true, matched)
+
+		matched, err = DataExpressionMatchPost(jsonWithPureIdExpression, model.Post{Content: "老王做空比特币"})
+		require.Nil(t, err)
+		require.Equal(t, false, matched)
+
+		matched, err = DataExpressionMatchPost(jsonWithPureIdExpression, model.Post{Content: "老王做空bitcoin"})
+		require.Nil(t, err)
+		require.Equal(t, true, matched)
+	})
+
+	t.Run("Empty expression should match anything", func(t *testing.T) {
+		matched, err := DataExpressionMatchPost(jsonWithEmptyExpression, model.Post{Content: "马斯克做空以太坊"})
+		require.Nil(t, err)
+		require.True(t, matched)
+
+		matched, err = DataExpressionMatchPost(jsonWithEmptyExpression, model.Post{Content: "随便一个字符串"})
+		require.Nil(t, err)
+		require.True(t, matched)
+
+		matched, err = DataExpressionMatchPost(jsonWithEmptyExpression, model.Post{Content: "马云马斯克马克扎克伯格"})
+		require.Nil(t, err)
+		require.True(t, matched)
 	})
 }
