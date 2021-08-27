af0b73ebca1957b37c8ccbd6972a6f8244dd3da2
Boning <boningga@usc.edu>
2021-08-26 18:54:48 -0700

    fix issue

--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -5,7 +5,7 @@ docker-compose up -d
 # postgres takes a sec to spin up and accept connections
 sleep 5
 
-docker exec base-postgres \
+docker exec newsdb \
   bash -c "cat /usr/src/dump.sql | psql \
     -U root \
     -d localdb" 
