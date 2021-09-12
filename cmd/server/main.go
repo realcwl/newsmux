e17003dd0ea716cf05071931f1692745ef9d4124
Jamie <wangqishere@gmail.com>
2021-09-12 16:06:05 -0700

    change to /api as API prefix, for loadbalancer routing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Luismorlan/newsmux/server"
 	"github.com/Luismorlan/newsmux/server/middlewares"
 	. "github.com/Luismorlan/newsmux/utils"
 	"github.com/Luismorlan/newsmux/utils/dotenv"
+	"github.com/Luismorlan/newsmux/utils/flag"
 	. "github.com/Luismorlan/newsmux/utils/flag"
 	. "github.com/Luismorlan/newsmux/utils/log"
 	"github.com/gin-contrib/cors"
@@ -46,26 +44,23 @@ func main() {
 	}
 
 	handler := server.GraphqlHandler()
-	router.POST("/graphql", handler)
-	router.GET("/subscription", handler)
+	router.POST("/api/graphql", handler)
+	router.GET("/api/subscription", handler)
 
-	router.GET("/healthcheck", server.HealthcheckHandler())
+	router.GET("/api/healthcheck", server.HealthcheckHandler())
 
 	// Setup graphql playground for debugging
-	router.GET("/", func(c *gin.Context) {
-		playground.Handler("GraphQL", "/graphql").ServeHTTP(c.Writer, c.Request)
-	})
-	router.GET("/sub", func(c *gin.Context) {
-		playground.Handler("Subscription", "/subscription").ServeHTTP(c.Writer, c.Request)
-	})
-
-	// TODO(chenweilunster): Keep this for now for fast debug. Remove this debug
-	// route once the application is fully implemented.
-	router.GET("/ping", func(c *gin.Context) {
-		fmt.Println(c.Request.Header.Get("sub"))
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	if flag.IsDevelopment {
+		router.GET("/playground", func(c *gin.Context) {
+			playground.Handler("GraphQL", "/api/graphql").ServeHTTP(c.Writer, c.Request)
 		})
+		router.GET("/playground/sub", func(c *gin.Context) {
+			playground.Handler("Subscription", "/api/subscription").ServeHTTP(c.Writer, c.Request)
+		})
+	}
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"message": "Newsfeed server - API not found"})
 	})
 
 	Log.Info("api server starts up")
