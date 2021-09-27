aa5e6cc0334dabe0dea9f95a64fd97889b0bcc6b
Jamie <wangqishere@gmail.com>
2021-09-26 17:23:43 -0700

    restructured code and added time to jin 10

--- collector/collector_test.go
+++ collector/collector_test.go
@@ -4,20 +4,62 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/Luismorlan/newsmux/protocol"
 	"github.com/Luismorlan/newsmux/utils/dotenv"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/html"
 )
 
 func TestMain(m *testing.M) {
 	dotenv.LoadDotEnvsInTests()
 	os.Exit(m.Run())
 }
+
+// Construct a HTMLElement according to html raw string and its id
+func GetMockHtmlElem(s string, id string) *colly.HTMLElement {
+	reader := strings.NewReader(s)
+	node, err := html.Parse(reader)
+	if err != nil {
+		panic(err)
+	}
+	var targetNode *html.Node
+
+	// find the html node with the specified id
+	// doing this because the node from html.Parse by default has <html><body> ... <your elem>... </body></html>
+	// need id to identify the elem
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		for _, a := range n.Attr {
+			if a.Key == "id" {
+				if a.Val == id {
+					targetNode = n
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(node)
+
+	doc := goquery.NewDocumentFromNode(targetNode)
+	elem := colly.NewHTMLElementFromSelectionNode(
+		&colly.Response{
+			Request: nil,
+		},
+		doc.Selection,
+		targetNode,
+		0)
+	return elem
+}
+
 func TestJin10Crawler(t *testing.T) {
-	var elem colly.HTMLElement
+	// var elem colly.HTMLElement
 	crawler := NewJin10Crawler()
 	taskId := "task_1"
 	sourceId := "source_1"
@@ -29,19 +71,21 @@ func TestJin10Crawler(t *testing.T) {
 			HeaderParams: []*protocol.KeyValuePair{},
 			Cookies:      []*protocol.KeyValuePair{},
 			SourceId:     sourceId,
-			SubSources:   []*protocol.PanopticSubSource{},
+			SubSources: []*protocol.PanopticSubSource{
+				{
+					Name: "快讯",
+					Type: protocol.PanopticSubSource_FLASHNEWS,
+				},
+			},
 		},
 		TaskMetadata: &protocol.TaskMetadata{},
 	}
 
-	t.Run("[get message from dom element][flash] html with header and <br/>", func(t *testing.T) {
-		htmlWithHeader := `<div data-v-c7711130="" data-v-471802f2="" id="flash20210926132904521100" class="jin-flash-item-container is-normal"><!----><div data-v-c7711130="" data-relevance="标普" class="jin-flash-item flash is-relevance"><div data-v-2b138c9f="" data-v-c7711130="" class="detail-btn"><div data-v-2b138c9f="" class="detail-btn_container"><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span></div></div><!----><div data-v-c7711130="" class="item-time">13:29:04</div><div data-v-c7711130="" class="item-right is-common"><div data-v-c7711130="" class="right-top"><!----><div data-v-c7711130="" class="right-content"><div data-v-c7711130=""><b>标普：美国经济正在降温 但仍具有弹性</b><br>美国经济已经有所降温，但仍然具有弹性。将美国2021年和2022年实际GDP增速预期分别调整至5.7%和4.1%，此前在6月报告中的预期分别为6.7%和3.7%。尽管美国经济仍处于过热状态，但随着夏季结束，美国经济已经开始降温。供应中断仍是美国经济放缓的主要原因，而德尔塔变种病毒现在是另一个拖累因素。目前的GDP预测仍将是1984年以来的最高水平。预计美联储将在12月开始缩减资产购买规模，并在2022年12月加息，随后分别在2023年和2024年加息两次。</div></div><!----><!----><!----></div></div><div data-v-47f123d2="" data-v-c7711130="" class="flash-item-share" style="display: none;"><a data-v-47f123d2="" href="javascript:void('openShare')" class="share-btn"><i data-v-47f123d2="" class="jin-icon iconfont icon-fenxiang"></i><span data-v-47f123d2="">分享</span></a><a data-v-47f123d2="" href="https://flash.jin10.com/detail/20210926132904521100" target="_blank"><i data-v-47f123d2="" class="jin-icon iconfont icon-xiangqing"></i><span data-v-47f123d2="">详情</span></a><a data-v-47f123d2="" href="javascript:void('copyFlash')"><i data-v-47f123d2="" class="jin-icon iconfont icon-fuzhi"></i><span data-v-47f123d2="">复制</span></a><!----></div></div></div>`
-		doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlWithHeader))
-		if err != nil {
-			panic("can't parse html into document")
-		}
-		elem.DOM = doc.Selection
-		msg := crawler.GetMessage(&task, &elem)
+	t.Run("[get message from dom element][flash] html with title and <br/>", func(t *testing.T) {
+		htmlWithTitle := `<div data-v-c7711130="" data-v-471802f2="" id="flash20210926132904521100" class="jin-flash-item-container is-normal"><!----><div data-v-c7711130="" data-relevance="标普" class="jin-flash-item flash is-relevance"><div data-v-2b138c9f="" data-v-c7711130="" class="detail-btn"><div data-v-2b138c9f="" class="detail-btn_container"><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span></div></div><!----><div data-v-c7711130="" class="item-time">13:29:04</div><div data-v-c7711130="" class="item-right is-common"><div data-v-c7711130="" class="right-top"><!----><div data-v-c7711130="" class="right-content"><div data-v-c7711130=""><b>标普：美国经济正在降温 但仍具有弹性</b><br>美国经济已经有所降温，但仍然具有弹性。将美国2021年和2022年实际GDP增速预期分别调整至5.7%和4.1%，此前在6月报告中的预期分别为6.7%和3.7%。尽管美国经济仍处于过热状态，但随着夏季结束，美国经济已经开始降温。供应中断仍是美国经济放缓的主要原因，而德尔塔变种病毒现在是另一个拖累因素。目前的GDP预测仍将是1984年以来的最高水平。预计美联储将在12月开始缩减资产购买规模，并在2022年12月加息，随后分别在2023年和2024年加息两次。</div></div><!----><!----><!----></div></div><div data-v-47f123d2="" data-v-c7711130="" class="flash-item-share" style="display: none;"><a data-v-47f123d2="" href="javascript:void('openShare')" class="share-btn"><i data-v-47f123d2="" class="jin-icon iconfont icon-fenxiang"></i><span data-v-47f123d2="">分享</span></a><a data-v-47f123d2="" href="https://flash.jin10.com/detail/20210926132904521100" target="_blank"><i data-v-47f123d2="" class="jin-icon iconfont icon-xiangqing"></i><span data-v-47f123d2="">详情</span></a><a data-v-47f123d2="" href="javascript:void('copyFlash')"><i data-v-47f123d2="" class="jin-icon iconfont icon-fuzhi"></i><span data-v-47f123d2="">复制</span></a><!----></div></div></div>`
+		elem := GetMockHtmlElem(htmlWithTitle, "flash20210926132904521100")
+		msg := crawler.GetMessage(&task, elem)
+		require.NotNil(t, msg)
 
 		require.Equal(t, "5ec724613633757afe672f136da46e37", msg.Post.DeduplicateId)
 		require.Equal(t, "标普：美国经济正在降温 但仍具有弹性\n美国经济已经有所降温，但仍然具有弹性。将美国2021年和2022年实际GDP增速预期分别调整至5.7%和4.1%，此前在6月报告中的预期分别为6.7%和3.7%。尽管美国经济仍处于过热状态，但随着夏季结束，美国经济已经开始降温。供应中断仍是美国经济放缓的主要原因，而德尔塔变种病毒现在是另一个拖累因素。目前的GDP预测仍将是1984年以来的最高水平。预计美联储将在12月开始缩减资产购买规模，并在2022年12月加息，随后分别在2023年和2024年加息两次。", msg.Post.Content)
@@ -50,16 +94,19 @@ func TestJin10Crawler(t *testing.T) {
 		require.Equal(t, "https://newsfeed-logo.s3.us-west-1.amazonaws.com/jin10.png", msg.Post.SubSource.AvatarUrl)
 		require.Equal(t, "快讯", msg.Post.SubSource.Name)
 		require.Equal(t, sourceId, msg.Post.SubSource.SourceId)
+
+		tm, _ := time.Parse("20060102-15:04:05", "20210926-13:29:04")
+		require.Equal(t, tm.Unix(), msg.Post.ContentGeneratedAt.AsTime().Unix())
 	})
 
 	t.Run("[get message from dom element][keynews] html with image", func(t *testing.T) {
+		task.TaskParams.SubSources[0].Name = "要闻"
+		task.TaskParams.SubSources[0].Type = protocol.PanopticSubSource_KEYNEWS
+
 		htmlWithImage := `<div data-v-c7711130="" data-v-471802f2="" id="flash20210925215015057100" class="jin-flash-item-container is-normal"><!----><div data-v-c7711130="" class="jin-flash-item flash is-important"><div data-v-2b138c9f="" data-v-c7711130="" class="detail-btn"><div data-v-2b138c9f="" class="detail-btn_container"><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span></div></div><!----><div data-v-c7711130="" class="item-time">21:50:15</div><div data-v-c7711130="" class="item-right is-common"><div data-v-c7711130="" class="right-top"><!----><div data-v-c7711130="" class="right-content"><div data-v-c7711130="">孟晚舟乘坐的中国政府包机抵达深圳宝安机场。欢迎回家！（人民日报）</div></div><div data-v-c7711130="" class="right-pic img-intercept flash-pic"><div data-v-c7711130="" class="img-container show-shadow"><img data-v-c7711130="" data-src="https://flash-scdn.jin10.com/16f8ddbe-1b4b-4c1b-a3d1-844e466edb67.jpg/lite" src="https://flash-scdn.jin10.com/16f8ddbe-1b4b-4c1b-a3d1-844e466edb67.jpg/lite" lazy="loaded"></div></div><!----><!----></div></div><div data-v-47f123d2="" data-v-c7711130="" class="flash-item-share" style="display: none;"><a data-v-47f123d2="" href="javascript:void('openShare')" class="share-btn"><i data-v-47f123d2="" class="jin-icon iconfont icon-fenxiang"></i><span data-v-47f123d2="">分享</span></a><a data-v-47f123d2="" href="https://flash.jin10.com/detail/20210925215015057100" target="_blank"><i data-v-47f123d2="" class="jin-icon iconfont icon-xiangqing"></i><span data-v-47f123d2="">详情</span></a><a data-v-47f123d2="" href="javascript:void('copyFlash')"><i data-v-47f123d2="" class="jin-icon iconfont icon-fuzhi"></i><span data-v-47f123d2="">复制</span></a><!----></div></div></div>`
-		doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlWithImage))
-		if err != nil {
-			panic("can't parse html into document")
-		}
-		elem.DOM = doc.Selection
-		msg := crawler.GetMessage(&task, &elem)
+		elem := GetMockHtmlElem(htmlWithImage, "flash20210925215015057100")
+		msg := crawler.GetMessage(&task, elem)
+		require.NotNil(t, msg)
 
 		require.Equal(t, "f8aa6e7b8619cb3730b0d3f291282740", msg.Post.DeduplicateId)
 		require.Equal(t, "孟晚舟乘坐的中国政府包机抵达深圳宝安机场。欢迎回家！（人民日报）", msg.Post.Content)
@@ -70,5 +117,19 @@ func TestJin10Crawler(t *testing.T) {
 		require.Equal(t, "要闻", msg.Post.SubSource.Name)
 		require.Equal(t, sourceId, msg.Post.SubSource.SourceId)
 
+		tm, _ := time.Parse("20060102-15:04:05", "20210925-21:50:15")
+		require.Equal(t, tm.Unix(), msg.Post.ContentGeneratedAt.AsTime().Unix())
+	})
+
+	t.Run("[get message from dom element][keynews] not request specified", func(t *testing.T) {
+		// Asking for Flash news
+		task.TaskParams.SubSources[0].Name = "快讯"
+		task.TaskParams.SubSources[0].Type = protocol.PanopticSubSource_FLASHNEWS
+
+		// Got Key news
+		htmlWithImage := `<div data-v-c7711130="" data-v-471802f2="" id="flash20210925215015057100" class="jin-flash-item-container is-normal"><!----><div data-v-c7711130="" class="jin-flash-item flash is-important"><div data-v-2b138c9f="" data-v-c7711130="" class="detail-btn"><div data-v-2b138c9f="" class="detail-btn_container"><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span><span data-v-2b138c9f=""></span></div></div><!----><div data-v-c7711130="" class="item-time">21:50:15</div><div data-v-c7711130="" class="item-right is-common"><div data-v-c7711130="" class="right-top"><!----><div data-v-c7711130="" class="right-content"><div data-v-c7711130="">孟晚舟乘坐的中国政府包机抵达深圳宝安机场。欢迎回家！（人民日报）</div></div><div data-v-c7711130="" class="right-pic img-intercept flash-pic"><div data-v-c7711130="" class="img-container show-shadow"><img data-v-c7711130="" data-src="https://flash-scdn.jin10.com/16f8ddbe-1b4b-4c1b-a3d1-844e466edb67.jpg/lite" src="https://flash-scdn.jin10.com/16f8ddbe-1b4b-4c1b-a3d1-844e466edb67.jpg/lite" lazy="loaded"></div></div><!----><!----></div></div><div data-v-47f123d2="" data-v-c7711130="" class="flash-item-share" style="display: none;"><a data-v-47f123d2="" href="javascript:void('openShare')" class="share-btn"><i data-v-47f123d2="" class="jin-icon iconfont icon-fenxiang"></i><span data-v-47f123d2="">分享</span></a><a data-v-47f123d2="" href="https://flash.jin10.com/detail/20210925215015057100" target="_blank"><i data-v-47f123d2="" class="jin-icon iconfont icon-xiangqing"></i><span data-v-47f123d2="">详情</span></a><a data-v-47f123d2="" href="javascript:void('copyFlash')"><i data-v-47f123d2="" class="jin-icon iconfont icon-fuzhi"></i><span data-v-47f123d2="">复制</span></a><!----></div></div></div>`
+		elem := GetMockHtmlElem(htmlWithImage, "flash20210925215015057100")
+		msg := crawler.GetMessage(&task, elem)
+		require.Nil(t, msg)
 	})
 }
--- collector/jin10.go
+++ collector/jin10.go
@@ -5,11 +5,13 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/Luismorlan/newsmux/protocol"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Jin10Crawler struct {
@@ -19,17 +21,17 @@ func NewJin10Crawler() *Jin10Crawler {
 	return &Jin10Crawler{}
 }
 
-func (collector Jin10Crawler) GetLevel(t interface{}) protocol.PanopticSubSource_SubSourceType {
-	if t.(*colly.HTMLElement).DOM.Find(".jin-flash-item").HasClass("is-important") {
+func (collector Jin10Crawler) GetLevel(elem *colly.HTMLElement) protocol.PanopticSubSource_SubSourceType {
+	if elem.DOM.Find(".jin-flash-item").HasClass("is-important") {
 		return protocol.PanopticSubSource_KEYNEWS
 	}
 	return protocol.PanopticSubSource_FLASHNEWS
 }
 
-// Process each html selection to get content
-func (collector Jin10Crawler) GetContent(s *goquery.Selection) string {
+func (collector Jin10Crawler) GetContent(task *protocol.PanopticTask, elem *colly.HTMLElement) string {
 	content := ""
-	s.Children().Each(func(i int, s *goquery.Selection) {
+	selection := elem.DOM.Find(".right-content > div")
+	selection.Children().Each(func(i int, s *goquery.Selection) {
 		if len(s.Nodes) > 0 && s.Nodes[0].Data == "br" {
 			content = content + "\n"
 		}
@@ -38,32 +40,31 @@ func (collector Jin10Crawler) GetContent(s *goquery.Selection) string {
 
 	// goquery don't have a good way to get text without child elements'
 	// remove children's text manually
-	remove := s.Children().Text()
-	text := s.Text()
+	remove := selection.Children().Text()
+	text := selection.Text()
 	result := strings.Replace(text, remove, "", -1)
 	content = content + result
 	return content
 }
 
-func (collector Jin10Crawler) GetMessage(task *protocol.PanopticTask, elem *colly.HTMLElement) *protocol.CrawlerMessage {
-	var requestedTypes map[protocol.PanopticSubSource_SubSourceType]bool
-
-	for _, subsource := range task.TaskParams.SubSources {
-		requestedTypes[subsource.Type] = true
+func (collector Jin10Crawler) GetGeneratedTime(task *protocol.PanopticTask, elem *colly.HTMLElement) time.Time {
+	id := elem.DOM.AttrOr("id", "")
+	timeText := elem.DOM.Find(".item-time").Text()
+	dateStr := id[5:13] + "-" + timeText
+	time, err := time.Parse("20060102-15:04:05", dateStr)
+	if err != nil {
+		return time.UTC()
 	}
+	return time
+}
 
-	level := collector.GetLevel(elem)
-	// todo: check requested level, return only requested
-	// if _, ok := requestedTypes[level]; !ok {
-	// 	return nil
-	// }
+func (collector Jin10Crawler) GetDedupId(task *protocol.PanopticTask, content string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(task.TaskParams.SourceId + content))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
 
-	fmt.Println("Time: ", elem.DOM.Find(".item-time").Text())
-	// todo: deal with time
-
-	s := elem.DOM.Find(".right-content > div")
-	content := collector.GetContent(s)
-
+func (collector Jin10Crawler) GetImageUrls(task *protocol.PanopticTask, elem *colly.HTMLElement) []string {
 	// there is only one image can be in jin10
 	imageUrl := elem.DOM.Find(".img-container > img").AttrOr("src", "")
 	var imageUrls []string
@@ -71,8 +72,41 @@ func (collector Jin10Crawler) GetMessage(task *protocol.PanopticTask, elem *coll
 		imageUrls = append(imageUrls, imageUrl)
 	}
 
-	hasher := md5.New()
-	hasher.Write([]byte(task.TaskParams.SourceId + content))
+	return imageUrls
+}
+
+// Process each html selection to get content
+func (collector Jin10Crawler) IsLegal(task *protocol.PanopticTask, level protocol.PanopticSubSource_SubSourceType) bool {
+	requestedTypes := make(map[protocol.PanopticSubSource_SubSourceType]bool)
+
+	for _, subsource := range task.TaskParams.SubSources {
+		s := subsource
+		requestedTypes[s.Type] = true
+	}
+
+	if _, ok := requestedTypes[level]; !ok {
+		fmt.Println("NOT LEGAL, actual level: ", level)
+		return false
+	}
+
+	return true
+}
+
+func (collector Jin10Crawler) GetMessage(task *protocol.PanopticTask, elem *colly.HTMLElement) *protocol.CrawlerMessage {
+
+	level := collector.GetLevel(elem)
+
+	if !collector.IsLegal(task, level) {
+		return nil
+	}
+
+	generatedTime := collector.GetGeneratedTime(task, elem)
+
+	content := collector.GetContent(task, elem)
+
+	deduplicateId := collector.GetDedupId(task, content)
+
+	imageUrls := collector.GetImageUrls(task, elem)
 
 	res := &protocol.CrawlerMessage{
 		Post: &protocol.CrawlerMessage_CrawledPost{
@@ -82,8 +116,8 @@ func (collector Jin10Crawler) GetMessage(task *protocol.PanopticTask, elem *coll
 				AvatarUrl: "https://newsfeed-logo.s3.us-west-1.amazonaws.com/jin10.png", //todo: put in central place
 			},
 			Content:            content,
-			ContentGeneratedAt: &timestamp.Timestamp{},
-			DeduplicateId:      hex.EncodeToString(hasher.Sum(nil)),
+			ContentGeneratedAt: timestamppb.New(generatedTime),
+			DeduplicateId:      deduplicateId,
 			ImageUrls:          imageUrls,
 		},
 		CrawledAt:      &timestamp.Timestamp{},
@@ -91,6 +125,7 @@ func (collector Jin10Crawler) GetMessage(task *protocol.PanopticTask, elem *coll
 		CrawlerVersion: "1",   // todo: actual version
 		IsTest:         false,
 	}
+
 	return res
 }
 
