0bd263d21cc58ea7cb65f845052f4ba99bfdb06e
Jamie <wangqishere@gmail.com>
2021-10-04 20:48:13 -0700

    remove grpc

---
+++ scripts/collector_example_input/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Luismorlan/newsmux/protocol"
+	"google.golang.org/protobuf/proto"
+)
+
+// use this script to generate a request you can use to send in Lambda->Test
+func main() {
+	job := protocol.PanopticJob{
+		Tasks: []*protocol.PanopticTask{
+			{
+				TaskId:          "123",
+				DataCollectorId: protocol.PanopticTask_COLLECTOR_JINSHI,
+				TaskParams: &protocol.TaskParams{
+					HeaderParams: []*protocol.KeyValuePair{},
+					Cookies:      []*protocol.KeyValuePair{},
+					SourceId:     "123",
+					SubSources: []*protocol.PanopticSubSource{
+						{
+							Name:       "快讯",
+							Type:       protocol.PanopticSubSource_FLASHNEWS,
+							ExternalId: "1",
+						},
+					},
+				},
+			},
+			{
+				TaskId:          "456",
+				DataCollectorId: protocol.PanopticTask_COLLECTOR_JINSHI,
+				TaskParams: &protocol.TaskParams{
+					HeaderParams: []*protocol.KeyValuePair{},
+					Cookies:      []*protocol.KeyValuePair{},
+					SourceId:     "456",
+					SubSources: []*protocol.PanopticSubSource{
+						{
+							Name:       "要闻",
+							Type:       protocol.PanopticSubSource_KEYNEWS,
+							ExternalId: "2",
+						},
+					},
+				},
+			},
+		}}
+	bytes, _ := proto.Marshal(&job)
+	fmt.Println("serializedJob ", bytes)
+}
