bdcf90177f0cc91d74aba27077f2e17dd8bbe36d
Jamie <wangqishere@gmail.com>
2021-10-04 20:17:49 -0700

    use prod docker ecr

--- cmd/collector/build_docker_and_upload.sh
+++ cmd/collector/build_docker_and_upload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Obtain credentials
-aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 213288384225.dkr.ecr.us-west-1.amazonaws.com/data_collector_simple_2
+aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 213288384225.dkr.ecr.us-west-1.amazonaws.com/data_collector
 
 # Build with build context as project root
-docker build -t data_collector_simple_2 -f Dockerfile ../../
+docker build -t data_collector -f Dockerfile ../../
 
-docker tag data_collector_simple_2:latest 213288384225.dkr.ecr.us-west-1.amazonaws.com/data_collector_simple_2:latest
+docker tag data_collector:latest 213288384225.dkr.ecr.us-west-1.amazonaws.com/data_collector:latest
 
-docker push 213288384225.dkr.ecr.us-west-1.amazonaws.com/data_collector_simple_2:latest
+docker push 213288384225.dkr.ecr.us-west-1.amazonaws.com/data_collector:latest
