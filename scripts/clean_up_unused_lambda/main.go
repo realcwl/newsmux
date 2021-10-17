ac157fc6d66a1b50e95880bee579b607b0d66760
Luismorlan <chenweilunster@gmail.com>
2021-10-17 16:05:17 -0700

    script for clean up unused collector lambda

---
+++ scripts/clean_up_unused_lambda/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/Luismorlan/newsmux/panoptic"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+)
+
+const (
+	STALE_TIME = 15 * time.Minute
+)
+
+func deleteLambdaFunctions(client *lambda.Client, ctx context.Context) int {
+	count := 0
+	res, err := client.ListFunctions(ctx, &lambda.ListFunctionsInput{})
+	if err != nil {
+		panic(err)
+	}
+	now := time.Now()
+	for _, f := range res.Functions {
+		if !strings.HasPrefix(*f.FunctionName, "data_collector_") {
+			continue
+		}
+		lastModifiedTime, err := time.Parse("2006-01-02T15:04:05-0700", *f.LastModified)
+		if err != nil {
+			panic(err)
+		}
+
+		if now.Sub(lastModifiedTime) > STALE_TIME {
+			_, err := client.DeleteFunction(ctx, &lambda.DeleteFunctionInput{
+				FunctionName: f.FunctionName,
+			})
+
+			if err != nil {
+				panic(err)
+			}
+			count++
+			fmt.Println("function deleted, name:", *f.FunctionName, "Created at:", *f.LastModified)
+		}
+	}
+	return count
+}
+
+func main() {
+	var client *lambda.Client
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(panoptic.AWS_REGION),
+	)
+	if err != nil {
+		panic(err)
+	}
+	client = lambda.NewFromConfig(cfg)
+
+	for deleteLambdaFunctions(client, ctx) > 0 {
+		fmt.Println("== still have Lambda function to clean up ==")
+	}
+}
