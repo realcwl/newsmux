18eefd4c2228a6e8d5c60163cab805e50ff09365
homer <sdi.gbn@gmail.com>
2021-10-27 22:09:29 -0700

    add basic bot with listing public feeds command

---
+++ cmd/bot/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/Luismorlan/newsmux/server"
+	"github.com/Luismorlan/newsmux/server/middlewares"
+	"github.com/Luismorlan/newsmux/utils/dotenv"
+	. "github.com/Luismorlan/newsmux/utils/flag"
+	. "github.com/Luismorlan/newsmux/utils/log"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	gintrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gin-gonic/gin"
+)
+
+func init() {
+	// Middlewares
+	middlewares.Setup()
+
+	Log.Info("api server initialized")
+}
+
+func cleanup() {
+	Log.Info("api server shutdown")
+}
+
+func main() {
+	flag.Parse()
+	defer cleanup()
+
+	if err := dotenv.LoadDotEnvs(); err != nil {
+		panic(err)
+	}
+
+	// Default With the Logger and Recovery middleware already attached
+	router := gin.Default()
+
+	router.Use(cors.Default())
+	router.Use(gintrace.Middleware(ServiceName))
+
+	handler := server.BotCommandHandler()
+	router.POST("/bot", handler)
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"message": "Newsfeed server - API not found"})
+	})
+
+	Log.Info("bot server starts up")
+	router.Run(":8080")
+}
--- server/handlers.go
+++ server/handlers.go
@@ -1,22 +1,95 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/Luismorlan/newsmux/model"
 	"github.com/Luismorlan/newsmux/server/graph/generated"
 	"github.com/Luismorlan/newsmux/server/resolver"
 	"github.com/Luismorlan/newsmux/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/slack-go/slack"
+	"gorm.io/gorm/clause"
 )
 
 // HealthCheckHandler returns 200 whenever server is up
 func HealthcheckHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "UP"})
+	}
+}
+
+type CommandForm struct {
+	Command string `form:"command" binding:"required"`
+}
+
+func BotCommandHandler() gin.HandlerFunc {
+	db, err := utils.GetDBConnection()
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	utils.DatabaseSetupAndMigration(db)
+
+	return func(c *gin.Context) {
+		var form CommandForm
+		c.Bind(&form)
+		switch form.Command {
+		case "/feeds":
+			var feeds []*model.Feed
+			if err := db.Preload(clause.Associations).Where("visibility = 'GLOBAL'").Find(&feeds).Error; err != nil {
+				c.JSON(http.StatusNotFound, gin.H{"text": "failed to get public feeds. please contact tech"})
+			}
+			sort.Slice(feeds, func(i, j int) bool {
+				return len(feeds[i].Subscribers) > len(feeds[j].Subscribers)
+			})
+
+			/* Building message body */
+			// subscribe section
+			divSection := slack.NewDividerBlock()
+			blocks := []slack.Block{divSection}
+
+			subscribeBtnText := slack.NewTextBlockObject("plain_text", "Subscribe", false, false)
+			subscribeBtnEle := slack.NewButtonBlockElement("", "click_me_123", subscribeBtnText)
+			for i := 0; i < len(feeds); i++ {
+				optionText := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*%s* \t _%s_ \t `%d`", feeds[i].Name, feeds[i].Creator.Name, len(feeds[i].Subscribers)), false, false)
+				optionSection := slack.NewSectionBlock(optionText, nil, slack.NewAccessory(subscribeBtnEle))
+				blocks = append(blocks, optionSection)
+			}
+
+			unsubscribeBtnText := slack.NewTextBlockObject("plain_text", "Unsubscribe", false, false)
+			unsubscribeBtnEle := slack.NewButtonBlockElement("", "click_me_123", unsubscribeBtnText)
+
+			// unsubscribe section
+			blocks = append(blocks, divSection)
+			// TODO(boning): this is a demo for unsubscribe, will delete it when we have real channel feed table
+			optionSample := slack.NewTextBlockObject("mrkdwn", "*恒大足球* \t _Jamie_ \t `5`", false, false)
+			optionSection := slack.NewSectionBlock(optionSample, nil, slack.NewAccessory(unsubscribeBtnEle))
+
+			blocks = append(blocks, optionSection)
+
+			msg := slack.NewBlockMessage(blocks...)
+			b, err := json.MarshalIndent(msg, "", "    ")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			c.Data(http.StatusOK, "application/json", b)
+
+		default:
+			c.JSON(http.StatusNotFound, gin.H{
+				"response_type": "ephemeral",
+				"text":          "Sorry, slash commando, that's an unknown command",
+			})
+		}
 	}
 }
 
@@ -33,8 +106,8 @@ func GraphqlHandler() gin.HandlerFunc {
 	utils.DatabaseSetupAndMigration(db)
 
 	h := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{
-		DB:             db,
-		SignalChans:    resolver.NewSignalChannels(),
+		DB:          db,
+		SignalChans: resolver.NewSignalChannels(),
 	}}))
 
 	h.AddTransport(transport.Websocket{
