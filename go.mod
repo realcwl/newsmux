0e84c521ce80877d8d74efe2cefc3e7f3508b4fd
Luismorlan <chenweilunster@gmail.com>
2021-11-06 19:25:04 -0700

    add a gRPC server for calculating simhash

--- go.mod
+++ go.mod
@@ -50,6 +50,7 @@ require (
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f
 	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1
 	gonum.org/v1/gonum v0.9.3
+	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/DataDog/dd-trace-go.v1 v1.33.0
 	gopkg.in/yaml.v2 v2.4.0
--- protocol/go_generate.go
+++ protocol/go_generate.go
@@ -9,3 +9,7 @@ package protocol
 // Crawler Publisher Message
 //go:generate protoc --go_out=. --go_opt=paths=source_relative crawler_publisher_message.proto --experimental_allow_proto3_optional
 //go:generate protoc --python_out=. crawler_publisher_message.proto --experimental_allow_proto3_optional
+
+// Deduplicator
+//go:generate python -m grpc_tools.protoc -I. --python_out=../deduplicator --grpc_python_out=../deduplicator deduplicator.proto --experimental_allow_proto3_optional
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative deduplicator.proto
