3ed033b5d5c602521748bdf959c3ae15bbf3fb4b
Luismorlan <chenweilunster@gmail.com>
2021-11-08 23:34:13 -0800

    call deduplicator

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"time"
 
 	"github.com/Luismorlan/newsmux/deduplicator"
+	"github.com/Luismorlan/newsmux/protocol"
 	. "github.com/Luismorlan/newsmux/publisher"
+	"github.com/Luismorlan/newsmux/utils"
 	. "github.com/Luismorlan/newsmux/utils"
 	"github.com/Luismorlan/newsmux/utils/dotenv"
 	. "github.com/Luismorlan/newsmux/utils/flag"
 	. "github.com/Luismorlan/newsmux/utils/log"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -19,6 +24,25 @@ const (
 	publishMaxBackOffSeconds float64 = 2.0
 	initialBackOff           float64 = 0.1
 )
+
+var (
+	serverAddr = flag.String("deduplicator_addr", "localhost:50051", "The server address in the format of host:port for deduplicator")
+)
+
+func getDeduplicatorClientAndConnection() (protocol.DeduplicatorClient, *grpc.ClientConn) {
+	if !utils.IsProdEnv() {
+		return deduplicator.FakeDeduplicatorClient{}, nil
+	}
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial deduplicator: %v", err)
+	}
+	client := protocol.NewDeduplicatorClient(conn)
+
+	return client, conn
+}
 
 func getNewBackOff(backOff float64) float64 {
 	if backOff == 0.0 {
@@ -42,13 +66,15 @@ func main() {
 		Log.Fatal("fail to connect database : ", err)
 	}
 
+	client, conn := getDeduplicatorClientAndConnection()
+
 	reader, err := NewSQSMessageQueueReader(crawlerPublisherQueueName, 20)
 	if err != nil {
 		Log.Fatal("fail initialize SQS message queue reader : ", err)
 	}
 
 	// Main publish logic lives in processor
-	processor := NewPublisherMessageProcessor(reader, db, deduplicator.FakeDeduplicatorClient{}, nil)
+	processor := NewPublisherMessageProcessor(reader, db, client, conn)
 
 	// Exponentially backoff on
 	backOff := 0.0
