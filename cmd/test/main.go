5a0202dac652f26bce72a3a60ab581c54805fb4d
homer <sdi.gbn@gmail.com>
2021-12-04 16:00:30 -0800

    make post_sent map thread safe

---
+++ cmd/test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+
+	"github.com/Luismorlan/newsmux/bot"
+)
+
+func main() {
+	bot.TimeBoundedPushPost(context.Background(), "186f5196-91f1-41e5-bee3-e30c816a8e50", "f03ed178-7f6a-44dc-9788-c9c1e0a84092")
+}
