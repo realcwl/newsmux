8dba4f5f069e4bc46e893ab1251a8df01dcd320d
homer <sdi.gbn@gmail.com>
2021-12-05 18:29:40 -0800

    fix build issue

--- publisher/processor.go
+++ publisher/processor.go
@@ -273,7 +273,7 @@ func (processor *CrawlerpublisherMessageProcessor) ProcessOneCralwerMessage(msg 
 			if _, ok := channelsPushed[c.Id]; ok {
 				continue
 			}
-			go bot.TimeBoundedPushPost(context.Background(), c.Id, post.Id)
+			go bot.TimeBoundedPushPost(context.Background(), c.WebhookUrl, *post)
 			channelsPushed[c.Id] = struct{}{}
 		}
 	}
