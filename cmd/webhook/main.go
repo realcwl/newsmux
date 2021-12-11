1679efb4b64760a3fad367ef8bde7c3c2fe44db5
Weilun Chen <chenweilunster@gmail.com>
2021-12-11 13:30:28 -0800

    Webhook collector (#204)
    * add health check
    
    * resolve webhook undefined issue

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	Twitter "github.com/Luismorlan/newsmux/collector/webhook/twitter"
 	Flag "github.com/Luismorlan/newsmux/utils/flag"
 	Logger "github.com/Luismorlan/newsmux/utils/log"
 	"github.com/gin-gonic/gin"
@@ -24,4 +25,11 @@ func main() {
 
 	Logger.Log.Info("===== Webhook Server Started =====")
 	router.Run(":7070")
+}
+
+func AddTwitterWebhook(rg *gin.RouterGroup) {
+	twitter := rg.Group("/twitter")
+
+	twitter.GET("/", Twitter.HandleTwitterCRC)
+	twitter.POST("/", Twitter.HandleTwitterMessage)
 }
