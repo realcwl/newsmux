f7eccd63dbdaa8d128ff1e9100aba1e5adce239c
Luismorlan <chenweilunster@gmail.com>
2021-12-11 11:55:42 -0800

    basic framework for twitter webhook

---
+++ cmd/webhook/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	Flag "github.com/Luismorlan/newsmux/utils/flag"
+	Logger "github.com/Luismorlan/newsmux/utils/log"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	Flag.ParseFlags()
+	Logger.InitLogger()
+
+	router := gin.Default()
+
+	// Add a debug route for testing
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusAccepted, "pong")
+	})
+
+	AddTwitterWebhook(router.Group("/webhook"))
+	// Additional webhooks should be added below this line
+
+	Logger.Log.Info("===== Webhook Server Started =====")
+	router.Run(":7070")
+}
